787eb8004b34fa8b84a2e5d3f2c231f4fe4b20f1
Denis Murphy <denismurp@gmail.com>
2023-01-13 16:52:33 +0000

    Demo iOS app which will be used for testing the CoreML model

---
+++ swift/GRU-CoreML/GRU-CoreML/GRUCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRUCoreMLApp.swift
+//  GRUCoreML
+//
+//  Created by Denis Murphy on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GRUCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
